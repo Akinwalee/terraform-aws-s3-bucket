c1acab99b48248389f98d8945c8887d85ebde00f
Akinwalee <Akinwalee>
2022-08-16 22:10:29 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region  = s3_region
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = s3_acl
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-bucket-today"
+}
